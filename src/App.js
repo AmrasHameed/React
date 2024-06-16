b47b40bc77d4b7b00a67ec2e52989567ac893956
AmrasHameed <amrazhameed@gmail.com>
2024-06-16 14:12:20 +0530

    Starting To Do

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Header from './components/Header';
+
 function App() {
+  const data = 'Amras';
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header val = {data}/>
+      <h1>Hello World</h1>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+  return (
+    <div className="header">
+      <h1>This is {props.val}</h1>
+    </div>
+  )
+}
+
+export default Header
